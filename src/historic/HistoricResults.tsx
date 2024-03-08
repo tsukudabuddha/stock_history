d618885140c205dbb711b1aea858dcf5336bc735
Andrew Tsukuda <andrewtsukuda@gmail.com>
2024-03-07 21:34:40 -0800

    use grid to display results

--- src/historic/HistoricResults.tsx
+++ src/historic/HistoricResults.tsx
@@ -1,19 +1,53 @@
+import { AllocationPerformance } from './Historic'
 import { Fragment } from "react/jsx-runtime"
 import { PortfolioResults } from "./Historic"
 import { formatCurrency, formatPercentage } from "./utils"
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import { ColDef } from 'ag-grid-community'
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { ValueFormatterParams } from 'ag-grid-community';
 
 type Params = {
   portfolioResults: PortfolioResults
 }
-export const HistoricResults = ({ portfolioResults }: Params) => {
-  return <>
-    <Fragment>{`Current Portfolio Value: ${portfolioResults.currentValue}`}</Fragment>
-    <div style={{flexDirection: "column"}}>
+
+export const HistoricResults = ({ portfolioResults : { allocationPerformances, currentValue } }: Params) => {
+  const colDefs: ColDef<AllocationPerformance>[] = [
     {
-      portfolioResults.allocationPerformances.map(allocation => 
-        <Fragment>{`${allocation.symbol} - ${formatPercentage(allocation.percentageChange)} ${formatCurrency(allocation.absoluteChange)}`}</Fragment>
-        )
-    }
-    </div>
-  </>
+       field: 'symbol',
+       valueFormatter: (params: ValueFormatterParams<AllocationPerformance, string>) => {
+           return params.value ?? "Unknown"
+       }
+   },
+   {
+        field: 'percentageChange',
+        headerName: 'Total Percentage Change',
+        valueFormatter: (params: ValueFormatterParams<AllocationPerformance, number>) => {
+            return params.value ? formatPercentage(params.value) : "Unknown"
+        }
+    },
+    {
+      field: 'absoluteChange',
+      headerName: 'Total Dollar Change',
+      valueFormatter: (params: ValueFormatterParams<AllocationPerformance, number>) => {
+          return params.value ? formatCurrency(params.value) : "Unknown"
+      }
+  },
+]
+
+  return (
+  <div style={{marginTop: 40}}>
+    <Fragment>{`Current Portfolio Value: ${formatCurrency(currentValue)}`}</Fragment>
+      <div
+        className="ag-theme-quartz" // applying the grid theme
+        style={{ height: 500 }} // the grid will fill the size of the parent container
+      >
+        <AgGridReact<AllocationPerformance>
+          rowData={allocationPerformances}
+          columnDefs={colDefs}
+        />
+      </div>
+  </div>
+  )
 }
